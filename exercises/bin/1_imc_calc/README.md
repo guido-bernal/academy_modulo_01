2b6c3bbc242bb04ba27c6c2c56edfc465fb5804c
Guido Bernal <guido.bernal@hotmail.com>
2022-09-25 18:34:38 -0300

    feat: imc calc

---
+++ exercises/bin/1_imc_calc/README.md
@@ -0,0 +1,13 @@
+IMC = PESO * ALTURA²
+
+1. Criar duas variáveis para guardar peso e a altura
+2. Aplicar a fórmula do IMC salvando o resultado numa variável
+3. Exibir o resultado na tela
+
+Casos:
+    Peso = 80kg, Altura = 1.70, IMC = 27.68
+    Peso = 50kg, Altura = 1.50, IMC = 22.22
+    Peso = 105kg, Altura = 1.80, IMC = 32.41
+
+Bônus
+    Como arredondar o resultado e mostrar somente 2 casas decimais?
---
+++ exercises/bin/1_imc_calc/imc_calc.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+String imcFormula(int peso, double altura) {
+  final double imcFormula = peso / pow(altura, 2);
+
+  return (imcFormula).toStringAsFixed(2);
+}
+
+void main() {
+  // Esperado: 27.68
+  print("Caso 1: " + imcFormula(80, 1.70));
+
+  // Esperado: 22.22
+  print("\nCaso 2: " + imcFormula(50, 1.50));
+
+  // Esperado: 32.41
+  print("\nCaso 3: " + imcFormula(105, 1.80));
+}
